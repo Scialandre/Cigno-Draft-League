bed2ebc0dbdd5ae606ecf3057c999fda08a50e00
Giovanni Cernuschi <s296831@studenti.polito.it>
2024-04-04 11:38:29 +0200

    added private_var_template.py

---
+++ private_var_template.py
@@ -0,0 +1,10 @@
+import logging
+
+botToken='botToken'
+botPrefix='botPrefix'
+botInviteLink='botInviteLink'
+
+
+admins = ('admin IDs as int (+0 ->)',0);
+masterChannel = 'master channel ID as int';
+loggerLevel = logging.DEBUG; #logging info level
